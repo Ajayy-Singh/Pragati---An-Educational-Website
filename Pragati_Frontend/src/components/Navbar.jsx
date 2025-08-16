6f6a403c982f8440e0a997bae6320928577362e4
Ajayy-Singh <ajaysingh1438970@gmail.com>
2025-08-16 17:45:34 +0530

    Initial commit

---
+++ Pragati_Frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'   
+import logo from '../assets/logo.webp'
+
+const Navbar = () => {
+  return (
+    <Nav className="navbar navbar-light bg-light">
+        <img className='mx-3' style={{ width: '50px', height: '50px' }} src={logo} alt="logo" />
+        <div className='d-flex mx-4'>
+        <input
+            type="text"
+            className="form-control me-3"
+            placeholder="Search..."
+            style={{ maxWidth: '200px' }}
+        />
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+        </div>
+    </Nav>
+  )
+}
+
+export default Navbar
---
+++ Pragati_Frontend/src/components/teacher/StudyMatrial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudyMatrial = () => {
+  return (
+    <div>StudyMatrial</div>
+  )
+}
+
+export default StudyMatrial
