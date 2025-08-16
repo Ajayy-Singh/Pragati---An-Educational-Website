a2c9e5777a3da20b7e11767a4a59c6cdea4e6be2
Harshwardhan Ahirwar <ahirwarharshwardhan2@gmail.com>
2025-08-16 23:07:04 +0530

    update

--- Pragati_Frontend/src/App.jsx
+++ Pragati_Frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import StudentDashboard from './components/student/StudentDashboard';
+import TeacherDashboard from './components/teacher/TeacherDashboard';
+import ParentDashboard from './components/parents/ParentDashboard';
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/student" element={<StudentDashboard/>} />
+        <Route path="/teacher" element={<TeacherDashboard/>} />
+        <Route path="/parent" element={<ParentDashboard />} />
+
       </Routes>
   );
 }
