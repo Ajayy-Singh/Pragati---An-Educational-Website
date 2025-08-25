54488b2363465421101ef45fd550781027b0f5a4
naman <guptanamang119@gmail.com>
2025-08-25 17:02:18 +0530

    add

--- Pragati_Backend/routes/authRoute.js
+++ Pragati_Backend/routes/authRoute.js
@@ -1,8 +1,19 @@
 import express from "express"
 import { signup,login } from "../controllor/authController.js"
+import User from "../models/User.js";
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login',login);
+
+router.get('/users', async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.json(users);
+    }
+    catch(e){
+        
+    }
+})
 
 export default router;
